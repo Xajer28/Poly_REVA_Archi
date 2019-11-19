246fb78487ad0e4d344a61a15167c7e28696498a
Xajer28 <xajer28@yahoo.fr>
2019-11-19 11:16:06 +0100

    access name/radio

--- Version 1.1/Selenium_Drafts/firefox_draft.py
+++ Version 1.1/Selenium_Drafts/firefox_draft.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 driver = webdriver.Firefox()
 driver.get("https://www.linuxpedia.fr/doku.php/flux_radio")
 # Récupérer les elements de taille h2
-elts = driver.find_element_by_xpath('//*[@id="chiffres"]')
+elts = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div')
 # Les afficher
 print(elts.text)
-
+# Fermer le webdriver
 driver.close()
