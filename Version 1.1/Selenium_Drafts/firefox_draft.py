3d71bd29609c34015086f486f31eb542fb087545
Xajer28 <xajer28@yahoo.fr>
2019-11-20 10:55:15 +0100

    create json radios

--- Version 1.1/Selenium_Drafts/firefox_draft.py
+++ Version 1.1/Selenium_Drafts/firefox_draft.py
@@ -1,27 +1,43 @@
 from selenium import webdriver
+import json
+
 driver = webdriver.Firefox()
 driver.get("https://www.linuxpedia.fr/doku.php/flux_radio")
 # --------------------------------------------------
 # Récupérer les elements de taille h2 (les lettres de classification)
-# elts = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div')
-# elts_titles=elts.find_elements_by_tag_name("h2")
+elts_cat = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div')
+elts_name_cat=elts_cat.find_elements_by_tag_name("h2")
+# for i in range(len(elts_name_cat)):
+#     print(elts_name_cat[i].text)
+#------------------------------------------------------------------------------------------
+# Recupérer les donnes des tableaux
+elts_data = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[2]")
+# -------------------------------------------------
+# Recupérer les titres de radios
+elts_titles = elts_data.find_elements_by_class_name("col0")
 # for i in range(len(elts_titles)):
 #     print(elts_titles[i].text)
-elts = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[2]")
-# -------------------------------------------------
-#Recupérer les titres de radios
-elts_titles = elts.find_elements_by_class_name("col0")
-for i in range(len(elts_titles)):
-    print(elts_titles[i].text)
 #----------------------------------------------------
 # Récupérer les liens de flux radios
-elts_links = elts.find_elements_by_class_name("col1")
-for i in range(len(elts_links)):
-    print(elts_links[i].text)
+elts_links = elts_data.find_elements_by_class_name("col1")
+# for i in range(len(elts_links)):
+#     print(elts_links[i].text)
 
-# Mettre en forme
-print(len(elts_links))
-print(len(elts_titles))
+#-------------------------------------------------------
+# Mettre en forme (en JSON)
+data = {}
+data['radios_linuxpedia']= []
+for j in range(len(elts_titles)):
+    name = elts_titles[j].text
+    link = elts_links[j].text
+    data['radios_linuxpedia'].append({
+        'name': name ,
+        'link' : link
+    })
+
+name_file = 'radios.json'
+with open('./radios_json/'+name_file, 'w') as outfile:
+    json.dump(data, outfile)
 
 # Fermer le webdriver
 driver.close()
--- Version 1.1/radionline/radionline/spiders/radio_spyder.py
+++ Version 1.1/radionline/radionline/spiders/radio_spyder.py
@@ -1,20 +1,43 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from selenium import webdriver
 
 class RadioSpyderSpider(scrapy.Spider):
     name = 'radio-spyder'
-    allowed_domains = ['https://www.linuxpedia.fr/doku.php/flux_radio']
+    allowed_domains = ['linuxpedia.fr']
     start_urls = ['http://https://www.linuxpedia.fr/doku.php/flux_radio/']
 
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+
     def parse(self, response):
-        # Print what the spider is doing
-        print(response.url)
-         # Get all the <a> tags
-        h2_selectors = response.xpath("//h2")
+        # # Print what the spider is doing
+        # print(response.url)
+        #  # Get all the <a> tags
+        # h2_selectors = response.xpath("//h2")
+
+        self.driver.get(response.url)
+        elts = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[2]")
+        # -------------------------------------------------
+        #Recupérer les titres de radios
+        elts_titles = elts.find_elements_by_class_name("col0")
+        # for i in range(len(elts_titles)):
+        #     print(elts_titles[i].text)
+        
+        #----------------------------------------------------
+        # Récupérer les liens de flux radios
+        elts_links = elts.find_elements_by_class_name("col1")
+        # for i in range(len(elts_links)):
+        #     print(elts_links[i].text)
+
+        # Mettre en forme
         #Print all h1
         # Loop on each tag
-        for selector in h2_selectors:
+        for i in range(len(elts_links)):
             yield {
-                'name' : selector
+                'name' : elts_titles[i].text,
+                'link' : elts_links[i].text
             }
+
+
+        self.driver.close()
