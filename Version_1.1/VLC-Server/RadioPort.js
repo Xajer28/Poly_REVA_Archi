a374a4f7147e70bd42ad310935bf0b65a4e42e87
Xajer28 <xajer28@yahoo.fr>
2019-12-11 21:07:35 +0100

    add vlc support + debug playport radio

--- Version_1.1/VLC-Server/RadioPort.js
+++ Version_1.1/VLC-Server/RadioPort.js
@@ -7,10 +7,13 @@ var radioPorts = require('express').Router(),
     jsdom = require('jsdom'),
     cp = require('child_process'),
     vlcCommand = require('vlc-command'),
-
     jsonpath = path.join(__dirname, '/', 'radios.json'),
     file=fs.readFileSync(jsonpath),
     radios=JSON.parse(file);
+
+const delay = require('delay');
+
+var Port_Radio_Play=8085;
 
 // A l'adresse /, on envoie le fichier index.html
 radioPorts.get('/',function(request,response){
@@ -18,41 +21,36 @@ radioPorts.get('/',function(request,response){
     response.render('index.ejs',{rows:radios});
 });
 
-
 //Récupération du lien par le port
-radioPorts.post('/',function(request,response){
+radioPorts.get('/radio',function(request,response){
     // var radiotitle=radios[req.body.numeroRadio].title;
-    console.log( request );
-    console.log("Radio clicked");
-    // console.log(radiotitle);
-
-    // vlcCommand(function (err, cmd) {
-    //     if (err) return console.error('could not find vlc command path')
-
-    //     if (process.platform === 'win32') {
-    //     cp.execFile(cmd, ['--version'], function (err, stdout) {
-    //         if (err) return console.error(err)
-    //         console.log(stdout)
-    //     })
-    //     } else {
-    //     cp.exec("vlc -vvv "+notreRadio+" --sout '#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100,scodec=none}:http{mux=mp3,dst=:"+nextPortNumber+"/}'", function (err, stdout) {
-    //         if (err) return console.error(err);
-    //         console.log(stdout);
-    //         console.log("Lancement de la Radio");
-    //     })
-    //     }
-    // })
-
-    // //Ouverture d'un port dès que la radio est sélectionnée
-    // var obj = {} // empty Object
-    // var key = 'PortNumber';
-    // //Attribution d'un port à l'objet
-    // obj[key] = RadioPortNumber;
-
-    // (async () => {
-    //     await delay(5000);
-    //     res.render('radio.ejs', { rows : obj });
-    //     RadioPortNumber+=1;
-    // })();
+    var link_radio = request.query.link;
+    console.log(link_radio);
+    vlcCommand(function (err, cmd) {
+        if (err) return console.error('could not find vlc command path')
+        if (process.platform === 'win32') {
+        cp.execFile(cmd, ['--version'], function (err, stdout) {
+            if (err) return console.error(err)
+            console.log(stdout)
+        })
+        } else {
+        cp.exec("vlc -vvv "+link_radio+" --sout '#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100,scodec=none}:http{mux=mp3,dst=:"+Port_Radio_Play+"/}'", function (err, stdout) {
+            if (err) return console.error(err);
+            console.log(stdout);
+            console.log("Lancement de la Radio");
+        })
+        }
+    })
+    
+    var o = {} // empty Object
+    var key = 'PortNumber';
+    o[key] = Port_Radio_Play;
+    (async () => {
+        await delay(5000);
+        response.json(o);
+        Port_Radio_Play+=1;
+    })();
+    console.log("Service Opened on http://127.0.0.1:"+Port_Radio_Play+"/");
+    
 });
 module.exports=radioPorts;
--- Version_1.1/VLC-Server/server.js
+++ Version_1.1/VLC-Server/server.js
@@ -3,8 +3,10 @@ var html=require('html'),
   express = require("express"),
   server = express(),
   http = require('http'),
+  cors=require('cors'),
   bodyParser=require('body-parser');
   RadioPort=require('./RadioPort');
+
 
 const fetch = require('node-fetch');
 const delay = require('delay');
@@ -12,6 +14,13 @@ const delay = require('delay');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
+server.use(cors());
+server.use(
+  bodyParser.urlencoded({
+    extended:true
+  })
+);
 server.use(RadioPort);
 
 server.listen(port, hostname, function(){
